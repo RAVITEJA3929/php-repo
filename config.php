067c4f4a8f9e33b3abff6eeec44aaa99828a3a57
root <root@ip-172-31-89-247.ec2.internal>
2025-06-26 10:21:07 +0000

    Transfer files from ltibbhackathon

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+
+$host = "devopsdb"; /* Host name */
+$user = "root"; /* User */
+$password = "admin123"; /* Password */
+$dbname = "customers"; /* Database name */
+
+$con = mysqli_connect($host, $user, $password,$dbname);
+// Check connection
+if (!$con) {
+ die("Connection failed: " . mysqli_connect_error());
+}
